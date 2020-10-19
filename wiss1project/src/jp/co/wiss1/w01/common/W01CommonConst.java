418d852bb16a8bc67158b5fa3268457e809afa5f
ride0106 <72718553+ride0106@users.noreply.github.com>
2020-10-19 17:18:24 +0900

    ride0106
    共通関数を修正

--- wiss1project/src/jp/co/wiss1/w01/common/W01CommonConst.java
+++ wiss1project/src/jp/co/wiss1/w01/common/W01CommonConst.java
@@ -1,13 +1,86 @@
 package jp.co.wiss1.w01.common;
 
+/**
+ * WISS1ツール定数クラス
+ *
+ * @since 2020.10.18
+ * @author wiss1
+ * @version 1.0.0
+ */
 public class W01CommonConst {
 
-	// 処理が正常完了した場合
-	public static final String SUCCESS = "0";
+    /**
+     * 戻り値（処理が正常完了した場合）
+     */
+    public static final String SUCCESS = "0";
 
-	// 処理が異常終了した場合
-	public static final String ERROR = "1";
+    /**
+     * 戻り値（処理が異常終了した場合）
+     */
+    public static final String ERROR = "1";
 
-	// マクロを呼び出すコマンド
-	public static final String COMMAND = "cmd /c cscript vbs\\W01ShapeEvidenceMacro.vbs";
+    /**
+     * CSVファイル拡張子
+     */
+    public static final String CONST_EXTENSION_CSV = "csv";
+
+    /**
+     * TSVファイル拡張子
+     */
+    public static final String CONST_EXTENSION_TSV = "tsv";
+
+    /**
+     * UTF-8
+     */
+    public static final String CONST_CHAR_CODE_UTF8 = "UTF-8";
+
+    /**
+     * カンマ
+     */
+    public static final String CONST_ST_COMMA = ",";
+
+    /**
+     * タブ
+     */
+    public static final String CONST_ST_TAB = "\t";
+
+    /**
+     * テーブル選択肢_01
+     */
+    public static final String TBL_NM_ONE = "01：社員情報";
+
+    /**
+     * テーブル選択肢_02
+     */
+    public static final String TBL_NM_TWO = "02：部署コード";
+
+    /**
+     * テーブル選択肢_03
+     */
+    public static final String TBL_NM_THREE = "03：役職コード";
+
+    /**
+     * テーブル名_社員情報
+     */
+    public static final String TBL_NM_EMPLOYEE = "t_employee_datas";
+
+    /**
+     * テーブル名_部署コード
+     */
+    public static final String TBL_NM_DIVISION = "division_code";
+
+    /**
+     * テーブル名_役職コード
+     */
+    public static final String TBL_NM_POST = "post_code";
+
+    /**
+     * SELECT文
+     */
+    public static final String TBL_SELECT_ALL = "SELECT * FROM ";
+
+    /**
+     * コマンド
+     */
+    public static final String COMMAND = "cmd /c cscript vbs\\W01ShapeEvidenceMacro.vbs";
 }
--- wiss1project/src/jp/co/wiss1/w01/common/W01CommonUtil.java
+++ wiss1project/src/jp/co/wiss1/w01/common/W01CommonUtil.java
@@ -1,56 +1,71 @@
 package jp.co.wiss1.w01.common;
 
+/**
+ * WISS1ツール共通クラス
+ *
+ * @since 2020.10.18
+ * @author wiss1
+ * @version 1.0.0
+ */
 public class W01CommonUtil {
 
-	static final String BF_STR = "%1";
-	static final String DLMTR = ":";
+    static final String BF_STR = "%1";
+    static final String DLMTR = ":";
 
-	protected enum MsgStr {
-		I01("I01", "%1は正常終了しました。"),
-		I02("I02", "%1が完了しました。"),
-		E01("E01", "%1は異常終了しました。"),
-		E02("E02", "%1に失敗しました。");
+    protected enum MsgStr {
+        I01("I01", "%1は正常終了しました。"),
+        I02("I02", "%1が完了しました。"),
+        I03("I03", "%1を入力ください："),
+        I04("I04", "%1を選択してください："),
+        I05("I05", "%1完了"),
+        E01("E01", "%1は異常終了しました。"),
+        E02("E02", "%1に失敗しました。"),
+        E03("E03", "%1が存在しません"),
+        E04("E04", "%1で入力してください");
 
-		// フィールド定義
-		private String id;
-		private String msgStr;
+        // フィールド定義
+        private String id;
+        private String msgStr;
 
-		// コンストラクタ
-		private MsgStr(String id, String msgStr) {
-			this.id = id;
-			this.msgStr = msgStr;
-		}
+        // コンストラクタ
+        private MsgStr(String id, String msgStr) {
+            this.id = id;
+            this.msgStr = msgStr;
+        }
 
-		// ID取得
-		public String getId() {
-			return id;
-		}
-		// メッセージ取得
-		public String getMsgStr() {
-			return msgStr;
-		}
-	}
+        // ID取得
+        public String getId() {
+            return id;
+        }
 
-	// 指定したIDよりメッセージを作成し出力するメソッド
-	// 第一引数：指定ID
-	// 第二引数以降：任意文字列（%1の箇所を置き換える、複数指定可）
-	// 正常完了時：true、異常時：falseを返却
-	public boolean outMessage (String msgId, final String ...strs) {
-		MsgStr msg = MsgStr.valueOf(msgId);
-		String id = msg.getId();
-		String message = msg.getMsgStr();
+        // メッセージ取得
+        public String getMsgStr() {
+            return msgStr;
+        }
+    }
 
-		for (String str : strs) {
-			if (str == null) {
-				// 任意文字列がNULLの場合、パラメータエラー
-				System.out.println("メッセージ出力パラメータエラー");
-				return false;
-			}
-			// 「ID：メッセージ」を生成
-			message = id + DLMTR + message.replaceFirst(BF_STR, str);
-		}
-		// メッセージ出力
-		System.out.println(message);
-		return true;
-	}
+    /**
+     * 指定したIDよりメッセージを作成し出力するメソッド
+     * @param msgId 指定ID
+     * @param strs 任意文字列（%1の箇所を置き換える、複数指定可）
+     * @return 正常完了時：true、異常時：falseを返却
+     */
+    public boolean outMessage(String msgId, final String... strs) {
+        MsgStr msg = MsgStr.valueOf(msgId);
+        String id = msg.getId();
+        String message = msg.getMsgStr();
+
+        for (String str : strs) {
+            if (str == null) {
+                // 任意文字列がNULLの場合、パラメータエラー
+                System.out.println("メッセージ出力パラメータエラー");
+                return false;
+            }
+            // 「ID：メッセージ」を生成
+            message = id + DLMTR + message.replaceFirst(BF_STR, str);
+        }
+        // メッセージ出力
+        System.out.println(message);
+        return true;
+    }
 }
