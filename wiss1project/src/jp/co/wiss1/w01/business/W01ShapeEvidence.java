20383f04ace16ec70dd06425604a6e0134fe6a90
so-nakamura <72678691+so-nakamura@users.noreply.github.com>
2020-10-20 10:58:19 +0900

    so-nakamura
    フォーマッターの適用

--- wiss1project/src/jp/co/wiss1/w01/business/W01ShapeEvidence.java
+++ wiss1project/src/jp/co/wiss1/w01/business/W01ShapeEvidence.java
@@ -8,55 +8,53 @@ import jp.co.wiss1.w01.common.W01CommonConst;
 import jp.co.wiss1.w01.common.W01CommonUtil;
 
 /**
-* エビデンス整形（EXCEL出力）するマクロを呼び出すクラス
-*
-* @since 2020/10/04
-* @author Soma Nakamura
-* @version 1.0.0
-*
-*/
-
+ * エビデンス整形（EXCEL出力）するマクロを呼び出すクラス
+ *
+ * @since 2020.10.04
+ * @author Soma Nakamura
+ * @version 1.0.0
+ */
 public class W01ShapeEvidence {
 
-	@SuppressWarnings("resource")
-	public String main() {
+    @SuppressWarnings("resource")
+    public String main() {
 
-		W01CommonUtil message = new W01CommonUtil();
+        W01CommonUtil message = new W01CommonUtil();
 
-		System.out.println("csvファイルもしくはtsvファイルの格納先（絶対パス）を入力してください：");
+        message.outMessage("I03", "csvファイルもしくはtsvファイルの格納先（絶対パス）");
 
-		Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-		//ファイルパスの取得
-		String str = sc.nextLine();
+        //ファイルパスの取得
+        String str = sc.nextLine();
 
-		//ファイルが存在するかのチェック
-		File file = new File(str);
-		if (file.exists()) {
-		} else {
-			System.out.println("ファイルが存在しません");
-			return W01CommonConst.ERROR;
-		}
+        //ファイルが存在するかのチェック
+        File file = new File(str);
+        if (file.exists()) {
+        } else {
+            message.outMessage("E03", "ファイル");
+            return W01CommonConst.ERROR;
+        }
 
-		//ファイル名をパラメータとしてマクロに渡す
-		try {
-			Runtime rt = Runtime.getRuntime();
-			Process process = rt.exec(W01CommonConst.COMMAND + " " + str);
-			int ret = process.waitFor();
-			if (ret == 0) {
-				message.outMessage("I02", "EXCELファイルへのエビデンス成型");
-				return W01CommonConst.SUCCESS;
-			} else {
-				message.outMessage("E02", "EXCELファイルへのエビデンス成型");
-				return W01CommonConst.ERROR;
-			}
+        //ファイル名をパラメータとしてマクロに渡す
+        try {
+            Runtime rt = Runtime.getRuntime();
+            Process process = rt.exec(W01CommonConst.COMMAND + " " + str);
+            int ret = process.waitFor();
+            if (ret == 0) {
+                message.outMessage("I02", "EXCELファイルへのエビデンス成型");
+                return W01CommonConst.SUCCESS;
+            } else {
+                message.outMessage("E02", "EXCELファイルへのエビデンス成型");
+                return W01CommonConst.ERROR;
+            }
 
-		} catch (IOException | InterruptedException ex) {
-			ex.printStackTrace();
-			message.outMessage("E02", "EXCELファイルへのエビデンス成型");
-			return W01CommonConst.ERROR;
-		}
+        } catch (IOException | InterruptedException ex) {
+            ex.printStackTrace();
+            message.outMessage("E02", "EXCELファイルへのエビデンス成型");
+            return W01CommonConst.ERROR;
+        }
 
-	}
+    }
 
 }
