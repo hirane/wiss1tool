f661955f520c44dff796a84a570bf7ad5817fc14
so-nakamura <72678691+so-nakamura@users.noreply.github.com>
2020-11-09 14:48:44 +0900

    so-nakamura

--- wiss1project/src/jp/co/wiss1/w01/business/W01ShapeEvidence.java
+++ wiss1project/src/jp/co/wiss1/w01/business/W01ShapeEvidence.java
@@ -1,6 +1,5 @@
 package jp.co.wiss1.w01.business;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -8,14 +7,20 @@ import jp.co.wiss1.w01.common.W01CommonConst;
 import jp.co.wiss1.w01.common.W01CommonUtil;
 
 /**
- * エビデンス整形（EXCEL出力）するマクロを呼び出すクラス
+ * エビデンス整形（EXCEL出力）するマクロの呼出しクラス
  *
- * @since 2020.10.04
- * @author Soma Nakamura
- * @version 1.0.0
+ * @since 2020/10/04
+ * @author s-nakamura
+ * @version 1.0
  */
 public class W01ShapeEvidence {
 
+    /**
+     * エビデンス整形（EXCEL出力）するマクロの呼出しの主処理
+     *
+     * @param なし
+     * @return String（0:正常終了 1:異常終了）
+     */
     @SuppressWarnings("resource")
     public String main() {
 
@@ -28,27 +33,25 @@ public class W01ShapeEvidence {
         //ファイルパスの取得
         String str = sc.nextLine();
 
-        //ファイルが存在するかのチェック
-        File file = new File(str);
-        if (file.exists()) {
-        } else {
+        //インプットファイルの確認
+        int result = W01CommonUtil.checkInputPath(str, W01CommonConst.CONST_EXTENSION_CSV);
+        if (result == W01CommonConst.FCHECK_ERROR_EXT) {
+            result = W01CommonUtil.checkInputPath(str, W01CommonConst.CONST_EXTENSION_TSV);
+            if (result == W01CommonConst.FCHECK_ERROR_EXT) {
+                message.outMessage("E04", "csvファイルもしくはtsvファイルの格納先（絶対パス）");
+                //異常終了
+                return W01CommonConst.ERROR;
+            }
+        } else if (result == W01CommonConst.FCHECK_ERROR_EXS) {
             message.outMessage("E03", "ファイル");
+            //異常終了
             return W01CommonConst.ERROR;
-        }
 
-        //空ファイルかどうかのチェック
-        if (file.length() == 0) {
-            message.outMessage("E02", "EXCELファイルへのエビデンス成型");
+        } else if (result == W01CommonConst.FCHECK_ERROR_EMP) {
+            message.outMessage("E03", "ファイル内にデータ");
+            //異常終了
             return W01CommonConst.ERROR;
-        }
 
-        //拡張子の確認
-        String name = file.getName();
-        String extension = name.substring(name.lastIndexOf("."));
-        if (extension.equals(".csv") || extension.equals(".tsv")) {
-        } else {
-            message.outMessage("E02", "EXCELファイルへのエビデンス成型");
-            return W01CommonConst.ERROR;
         }
 
         //ファイル名をパラメータとしてマクロに渡す
@@ -56,17 +59,20 @@ public class W01ShapeEvidence {
             Runtime rt = Runtime.getRuntime();
             Process process = rt.exec(W01CommonConst.COMMAND + " " + str);
             int ret = process.waitFor();
-            if (ret == 0) {
+            if (ret == W01CommonConst.NUM_ZERO) {
                 message.outMessage("I02", "EXCELファイルへのエビデンス成型");
+                //正常終了
                 return W01CommonConst.SUCCESS;
             } else {
                 message.outMessage("E02", "EXCELファイルへのエビデンス成型");
+                //異常終了
                 return W01CommonConst.ERROR;
             }
 
         } catch (IOException | InterruptedException ex) {
             ex.printStackTrace();
-            message.outMessage("E02", "EXCELファイルへのエビデンス成型");
+            message.outMessage("E02", "マクロの呼び出し");
+            //異常終了
             return W01CommonConst.ERROR;
         }
 
