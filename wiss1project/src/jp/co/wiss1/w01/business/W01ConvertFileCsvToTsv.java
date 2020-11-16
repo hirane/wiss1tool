d93c65c8d265da960e3122e9f7dac4a4627f5001
nishikawa-miyuko <72734878+nishikawa-miyuko@users.noreply.github.com>
2020-11-16 16:05:30 +0900

    m-nishikawa

--- wiss1project/src/jp/co/wiss1/w01/business/W01ConvertFileCsvToTsv.java
+++ wiss1project/src/jp/co/wiss1/w01/business/W01ConvertFileCsvToTsv.java
@@ -40,28 +40,27 @@ public class W01ConvertFileCsvToTsv {
 			String createFile = inputPath.replace(W01CommonConst.CONST_EXTENSION_CSV,
 					W01CommonConst.CONST_EXTENSION_TSV);
 
+			// CSVファイルの読み込み
+			File fInputCsv = new File(inputPath);
+			BufferedReader br = new BufferedReader(new FileReader(fInputCsv));
+
 			// ファイルがあるかの確認：無い場合⇒エラーを返す
 			//File file = new File(inputPath);
 			//if (file.exists()) {
 			//} else {
-				//message.outMessage("I03", "正しい格納先（絶対パス）");
-				//return W01CommonConst.ERROR;
+			//message.outMessage("I03", "正しい格納先（絶対パス）");
+			//return W01CommonConst.ERROR;
 			//}
-
-			// CSVファイルの読み込み
-			File fInputCsv = new File(inputPath);
-			BufferedReader br = new BufferedReader(new FileReader(fInputCsv));
 
 			//読み込んだファイルの拡張子の判断：TSVファイルを入力された場合⇒エラーを返す
 			//String name = fInputCsv.getName();
 			//String extension = name.substring(name.lastIndexOf(W01CommonConst.CONST_ST_PERIOD));
 
 			//if (extension.equals(W01CommonConst.CONST_EXTENSION_TSV)) {
-				//br.close();
-				//message.outMessage("I03", "CSVファイル");
-				//return W01CommonConst.ERROR;
+			//br.close();
+			//message.outMessage("I03", "CSVファイル");
+			//return W01CommonConst.ERROR;
 			//}
-
 
 			//ファイルのチェックメソッド （0：正常、1：拡張子エラー、2:ファイルなしエラー、3:ファイル0バイトエラー）
 			String extension = W01CommonConst.CONST_EXTENSION_CSV;
@@ -69,10 +68,10 @@ public class W01ConvertFileCsvToTsv {
 			if (W01CommonUtil.checkInputPath(inputPath, extension) == W01CommonConst.FCHECK_ERROR_EXT) {
 				message.outMessage("I03", extension + "ファイル");
 				return W01CommonConst.ERROR;
-			}else if(W01CommonUtil.checkInputPath(inputPath, extension) == W01CommonConst.FCHECK_ERROR_EXS) {
+			} else if (W01CommonUtil.checkInputPath(inputPath, extension) == W01CommonConst.FCHECK_ERROR_EXS) {
 				message.outMessage("I03", "正しい格納先（絶対パス）");
 				return W01CommonConst.ERROR;
-			}else if(W01CommonUtil.checkInputPath(inputPath, extension) == W01CommonConst.FCHECK_ERROR_EMP) {
+			} else if (W01CommonUtil.checkInputPath(inputPath, extension) == W01CommonConst.FCHECK_ERROR_EMP) {
 				message.outMessage("E03", "ファイル内にデータ");
 				return W01CommonConst.ERROR;
 			}
